3b630be4cfd667fb998ce30c25f3c6a83c30eb9f
alexanderpiers <apiers@uw.edu>
2020-06-25 17:18:06 -0700

    add - cmake and xml config files

---
+++ topMetalDrone.cpp
@@ -0,0 +1,16 @@
+// Standard headers
+#include <vector>
+#include <iostream>
+
+// xml parsing headers
+#include "tinyxml2.h"
+
+
+int main(int argc, char const *argv[])
+{
+	/* code */
+	std::printf("Hello world, this is the introduction to the Top-metal Drone code\n");
+
+
+	return 0;
+}
